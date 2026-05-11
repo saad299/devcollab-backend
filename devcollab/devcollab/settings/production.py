6cd51286d59b9650abf63aadb8bdccc674977861
saad299 <msaadahmad421@gmail.com>
2026-05-12 01:44:36 +0500

    DevCollab requirements.txt, production.py, railway.json changed

--- devcollab/devcollab/settings/production.py
+++ devcollab/devcollab/settings/production.py
@@ -10,9 +10,9 @@ DATABASES = {
     )
 }
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
 
-CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', cast=Csv())
+CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', default='http://localhost:3000', cast=Csv())
 
 CORS_ALLOW_CREDENTIALS = True
 
