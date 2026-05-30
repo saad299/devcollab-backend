b9ae606f7d6c7c209699d84236ec4408f8b78271
saad299 <msaadahmad421@gmail.com>
2026-05-31 01:49:20 +0500

    fixed jwt. for real this time

--- devcollab/devcollab/settings/base.py
+++ devcollab/devcollab/settings/base.py
@@ -86,8 +86,8 @@ REST_FRAMEWORK = {
 
 from datetime import timedelta
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=42),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
 }
